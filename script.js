125033556efbe11ee927a416dbaadc7835a67ec9
Lígia Bicalho <ligiabicalho@gmail.com>
2022-08-03 13:35:38 -0300

    Requisitos 8 e 9

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const colorPalette = document.querySelector('#color-palette');
 colorPalette.addEventListener('click', modifyColorSelected);
 
 function modifyPixelColor(event) {
-  const classPixel = document.querySelector('.pixel');
+  // const classPixel = document.querySelector('.pixel');
   const colorSelectedClass = document.querySelector('.selected').classList[1];
   const pixelSelected = event.target;
   pixelSelected.classList.add(colorSelectedClass);
@@ -18,3 +18,20 @@ function modifyPixelColor(event) {
 
 const pixelBoard = document.querySelector('#pixel-board');
 pixelBoard.addEventListener('click', modifyPixelColor);
+
+function clearBoard() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].classList.remove('black');
+    pixels[index].classList.remove('red');
+    pixels[index].classList.remove('green');
+    pixels[index].classList.remove('blue');
+  }
+  // colorRemove = colorRemove[0];
+  // colorRemove.classList.remove(colorRemove);
+
+  // console.log();
+}
+
+const btn = document.querySelector('#clear-board');
+btn.addEventListener('click', clearBoard);
